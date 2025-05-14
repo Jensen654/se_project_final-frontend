63b5480237e4bb6128d433791b22701399db4aa7
Jensen Bean <jensen.bean@gmail.com>
2025-05-14 13:26:49 -0600

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import reactLogo from "../assets/react.svg";
+import "./blocks/App.css";
+import { Route, Routes } from "react-router";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        {/* header */}
+        {/* main */}
+        {/* footer */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./blocks/Header.css";
+
+function header() {
+  return (
+    <div className="header">
+      <img src="#" alt="Logo" />
+      <div className="header__profile"></div>
+    </div>
+  );
+}
+
+export default header;
