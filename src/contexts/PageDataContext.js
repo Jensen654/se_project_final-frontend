3c34c7deb44dc0e53b38fc459a510f34fad1f2c4
Jensen Bean <jensen.bean@gmail.com>
2025-06-03 23:08:41 -0600

    gh-pages

--- src/contexts/PageDataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const PageDataContext = createContext();
-
-export default PageDataContext;
--- src/contexts/PokeDataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const PokeDataContext = createContext();
-
-export default PokeDataContext;
